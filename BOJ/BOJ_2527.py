3c4b8f0784f4e6f67f54802cfaefc577cc6d29bc
summer <qwer62636275@gmail.com>
2021-08-22 16:33:12 +0900

    BOJ_2527

---
+++ BOJ/BOJ_2527.py
@@ -0,0 +1,20 @@
+for _ in range(4):
+    a, b, c, d, e, f, g, h = map(int, input().split())
+    lst = [(a, b, c, d), (e, f, g, h)]
+    lst.sort()
+    x1, y1, p1, q1 = lst[0]
+    x2, y2, p2, q2 = lst[1]
+    result = 'd'
+    if p1 > x2:
+        if y1 <= y2 < q1 or y1 < q2 <= q1 or (y2 < y1 and q2 > q1):
+            result = 'a'
+        elif y2 == q1 or q2 == y1:
+            result = 'b'
+    elif p1 == x2:
+        if y2 == q1 or q2 == y1:
+            result = 'c'
+        else:
+            result = 'b'
+    print(result)
+    
+
