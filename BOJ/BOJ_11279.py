08ae662b15fc571e786504754a91cb1cbdd06860
summer <qwer62636275@gmail.com>
2021-09-09 00:10:57 +0900

    Add 0908 last

---
+++ BOJ/BOJ_11279.py
@@ -0,0 +1,12 @@
+import sys
+import heapq as hq
+
+N = int(sys.stdin.readline())
+heap = []
+for _ in range(N):
+    X = int(sys.stdin.readline())
+    if X :
+        hq.heappush(heap,(-X,X))
+    else:
+        if len(heap)>=1:print(hq.heappop(heap)[1])
+        else:print(0)
