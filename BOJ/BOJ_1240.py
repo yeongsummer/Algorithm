028ec1df5913557743248afea75ca46394e946c2
summer <qwer62636275@gmail.com>
2021-10-06 01:42:31 +0900

    Add 1006

--- BOJ/BOJ_1240.py
+++ BOJ/BOJ_1240.py
@@ -1,23 +1,23 @@
 from collections import defaultdict, deque
 
 def bfs(n, m):
-    visited = []
     deq = deque()
     deq.append([n, 0])
+    visited[n] = 1
     while deq:
         n, d = deq.popleft()
-        if n not in visited:
-            visited.append(n)
-            for item in node[n]:
-                new_d = d+item[1]
-                if item[0] == m:
-                    return new_d
-                else:
+        for item in node[n]:
+            new_d = d+item[1]
+            if item[0] == m:
+                return new_d
+            else:
+                if visited[item[0]] == 0:
+                    visited[item[0]] = 1
                     deq.append((item[0], new_d))
 
 N, M = map(int, input().split())
 node = defaultdict(list)
-dist = {}
+
 for _ in range(N-1):
     n1, n2, d = map(int, input().split())
     node[n1].append((n2, d))
@@ -25,6 +25,6 @@ for _ in range(N-1):
 
 for _ in range(M):
     n1, n2 = map(int, input().split())
-    visited = []
+    visited = [0] * (N+1)
     print(bfs(n1, n2))
 
