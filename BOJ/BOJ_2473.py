794419c6eb4b3b80a88b2783f56973fbb00a7b44
summer <qwer62636275@gmail.com>
2021-10-07 00:44:00 +0900

    Add 1007 first

--- BOJ/BOJ_2473.py
+++ BOJ/BOJ_2473.py
@@ -1,24 +1,37 @@
 # 세 용액
-def two_pointer(lst):
-    left, right = 0, N-1
-    cnt = 0
-    tot = 0
- 
-    while left < len(a):
-        if tot == 0:
-            cnt += 1
-            tot -= a[left]
-            left += 1
-        elif tot > 0 or right >= len(a): # 이 부분율 유심히 보길 바란다.
-            tot += a[left]
-            left += 1
-        elif tot < target:
-            tot += a[right]
-            right += 1
- 
-    return cnt
+def two_pointer(lst, start, end):
+    global mark, answer_sum, flag
+    left, right = start, end
 
+    while left < right:
+        total = mark + lst[left] + lst[right]
+
+        if abs(total) < answer_sum:
+            answer[0] = mark
+            answer[1] = lst[left]
+            answer[2] = lst[right]
+            answer_sum = abs(total)
+                
+            if total == 0:
+                flag = True
+                break
+        
+        if total > 0:
+            right -= 1
+        elif total < 0:
+            left += 1
+    
+    
 N = int(input())
 nums = list(map(int,input().split()))
 nums.sort()
+answer_sum = float('inf')
+answer = [0]*3
+flag = False
+for i in range(N-2):
+    mark = nums[i]
+    two_pointer(nums, i+1, N-1)
+    if flag:
+        break
+print(*answer)
 
