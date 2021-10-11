5d7653496d9085ac85245a36f7a86a7b52329e60
summer <qwer62636275@gmail.com>
2021-10-11 16:57:46 +0900

    Add 1011

--- BOJ/BOJ_2527.py
+++ BOJ/BOJ_2527.py
@@ -6,14 +6,16 @@ for _ in range(4):
     x2, y2, p2, q2 = lst[1]
     result = 'd'
     if p1 > x2:
-        if y1 <= y2 < q1 or y1 < q2 <= q1 or (y2 < y1 and q2 > q1):
+        if y1 <= y2 < q1:
+            result = 'a'
+        elif y2 < y1 and q2 > y1:
             result = 'a'
         elif y2 == q1 or q2 == y1:
             result = 'b'
     elif p1 == x2:
         if y2 == q1 or q2 == y1:
             result = 'c'
-        else:
+        elif y2 < p1 and y1 < q2:
             result = 'b'
     print(result)
     
