9429c88b2383e04a1400d8b01eb6e43e294a9e25
summer <qwer62636275@gmail.com>
2021-10-31 00:30:03 +0900

    Add 1030

--- BOJ/BOJ_14719.py
+++ BOJ/BOJ_14719.py
@@ -1,42 +1,21 @@
 H, W = map(int, input().split())
-blocks = ''.join(input().split()).strip('0')
-blocks = list(map(int, list(blocks)))
+blocks = list(map(int, input().split()))
+start = 0
+end = len(blocks)-1
+
+while True:
+    if blocks[start] > blocks[start+1]:
+        break
+    start += 1
+
+while end:
+    if blocks[end] > blocks[end-1]:
+        break
+    end -= 1
 
 total = 0
-# b = blocks.pop()
-# while blocks:
-#     if b > blocks[-1]:
-#         total += b - blocks.pop()
-#     else:
-#         break
-# print(blocks)
-# b = blocks.pop(0)
-# for block in blocks:
-#     if b > block:
-#         total += b - block
-#     else:
-#         b = block
-
-l_block, r_block = blocks[0], blocks[-1]
-max_block = min(l_block, r_block)
-print(max_block)
-left, right = 1, len(blocks)-2
-
-
-while left < right:
-    print(left, right)
-
-    if l_block > blocks[left ]:
-        total += min(max_block,l_block) - blocks[left]
-    else:
-        l_block = blocks[left]
-    
-    if r_block > blocks[right]:
-        total += min(max_block,r_block) - blocks[right]
-    else:
-        l_block = blocks[right]
-    print(total)
-    left += 1
-    right -= 1
-
+for i in range(start+1, end):
+    max_block = min(max(blocks[:i]), max(blocks[i+1:]))
+    if blocks[i] < max_block:
+        total += max_block - blocks[i]
 print(total)
