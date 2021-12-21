2ccf93bf413243fd1a845bd56d4ec7a02aa07b81
summer <qwer62636275@gmail.com>
2021-12-21 21:53:21 +0900

    Add12/21

--- Programmers/Weekly_Challenge_12.py
+++ Programmers/Weekly_Challenge_12.py
@@ -1,23 +1,21 @@
 from itertools import permutations
 
 def solution(k, dungeons):
-    def game(n, lst, p):
-        nonlocal ans
+    def game(n, p):
+        nonlocal ans, lst
+
         if n > ans:
             ans = n
+
         if n == C:
             return
+
         if p >= lst[n][0]:
-            game(n+1, lst, p - lst[n][1])
-            
-    ans = 0  
+            game(n+1, p - lst[n][1])
+        
+    ans = 0
     C = len(dungeons)
     for lst in permutations(dungeons, C):
-        print(lst)
-        game(0, lst, k)
+        game(0, k)
 
     return ans
-
-k = 80
-dungeons = [[80,20],[50,40],[30,10]]
-print(solution(k, dungeons))
