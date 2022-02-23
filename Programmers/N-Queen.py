8f1fceb97be378a4d530ab5039082eb918336fca
yeongsummer <qwer62636275@gmail.com>
2022-02-24 01:07:30 +0900

    Add 02/24

--- Programmers/N-Queen.py
+++ Programmers/N-Queen.py
@@ -1,22 +1,3 @@
-def is_available(queens, c, r):
-    for i in range(len(queens)):
-        if queens[i] == c or abs(queens[i] - i) == abs(r - c):
-            return False
-    return True
-
-
-def dfs(answer, N, r, queens):
-    if r == N:
-        answer += 1
-        return
-    
-    for c in range(N):
-        if is_available(queens, c, r):
-            queens.append(c)
-            dfs(answer, N, r+1, queens)
-            queens.pop()
-
-    
 def solution(n):
     answer = 0
 
@@ -39,7 +20,6 @@ def solution(n):
                 queens.append(c)
                 dfs(N, r+1, queens)
                 queens.pop()
-
 
     dfs(n, 0, [])
     return answer
