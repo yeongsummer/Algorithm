ffe05b46a4abf52ba10a7b32260dd2dded09d810
yeongsummer <qwer62636275@gmail.com>
2022-03-31 23:52:31 +0900

    Add 03/31

--- BOJ/BOJ_2580.py
+++ BOJ/BOJ_2580.py
@@ -1,25 +1,28 @@
-sudoku = [list(map(int, input().split())) for _ in range(9)]
-nums = [1,2,3,4,5,6,7,8,9]
+from copy import deepcopy
 
-flag = True
-while flag:
+def sudoku(board):
+    global answer, find
+
+    if find:
+        return
+
     flag = False
-
     for x in range(9):
         for y in range(9):
-            if not sudoku[x][y]:
+            if not board[x][y]:
+                flag = True
                 # 가로
                 copy_nums = nums[:]
                 for i in range(9):
-                    if sudoku[x][i]:
-                        copy_nums.remove(sudoku[x][i])
+                    if board[x][i]:
+                        copy_nums.remove(board[x][i])
                 cand1 = copy_nums
 
                 # 세로
                 copy_nums = nums[:]
                 for i in range(9):
-                    if sudoku[i][y]:
-                        copy_nums.remove(sudoku[i][y])
+                    if board[i][y]:
+                        copy_nums.remove(board[i][y])
                 cand2 = copy_nums
 
                 # 사각형
@@ -27,15 +30,34 @@ while flag:
                 nx, ny = x // 3, y // 3
                 for i in range(3):
                     for j in range(3):
-                        if sudoku[nx*3+i][ny*3+j]:
-                            copy_nums.remove(sudoku[nx*3+i][ny*3+j])
+                        if board[nx*3+i][ny*3+j]:
+                            copy_nums.remove(board[nx*3+i][ny*3+j])
                 cand3 = copy_nums
 
                 cand = set(cand1) & set(cand2) & set(cand3)
 
-                if len(cand) == 1:
-                    sudoku[x][y] = list(cand)[0]
-                    flag = True
+                if not cand:
+                    return
+                
+                for i in cand:
+                    copy_board = deepcopy(board)
+                    copy_board[x][y] = i
+                    sudoku(copy_board)
+
+                break
+        if flag:
+            break
+    
+    if not flag:
+        find = 1
+        answer = board
+        return
+
+board = [list(map(int, input().split())) for _ in range(9)]
+nums = [1,2,3,4,5,6,7,8,9]
+answer = []
+find = 0
+sudoku(board)
 
 for i in range(9):
-    print(*sudoku[i])
+    print(*answer[i])
